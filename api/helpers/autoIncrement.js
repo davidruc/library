43fc8d6c64ace8f9a99c2c49f47e8fae9b27a4a7
davidruc <davidarueda18@gmail.com>
2023-09-23 13:45:21 -0500

    feat: :sparkles: creation of the autoincrement function

---
+++ api/helpers/autoIncrement.js
@@ -0,0 +1,11 @@
+import { collectionGen } from "../db/connection.js";
+
+export default async function siguienteId(coleccion){
+    let db = await collectionGen("counters");
+    const sequenceDocument = await db.findOneAndUpdate(
+        { counter: `${coleccion}Id` },
+        { $inc: { sequence_value: 1 } },
+        { returnDocument: "after" }
+    );
+    return sequenceDocument.value.sequence_value;
+}

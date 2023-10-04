2cdf6dc600e8249b9448c0c7bc486ec37ae4fb9e
davidruc <davidarueda18@gmail.com>
2023-10-04 09:27:49 -0500

    fix: :bug: arreglando un problema en la carga de la pagina cuando no alcanzaba a cargar

--- src/components/books/books.jsx
+++ src/components/books/books.jsx
@@ -8,7 +8,7 @@ import defaultImage from "../../assets/bookDefault.png"
 import "./books.css";
 export default function Books() {
     const navigate = useNavigate();
-    const { token,getToken } = useToken();
+    const { getToken } = useToken();
     const [datos, setDatos] = useState([])
     const [loading, setLoading] = useState(true);
     const handleDataFetched = (response) => {
@@ -26,7 +26,10 @@ export default function Books() {
     },[])
     useEffect(() => {
         async function getAviableBooks() {
+            const access = await getToken();
+            const token = await access.token;
             try {
+                setLoading(true);
                 const res = await fetch(`http://${import.meta.env.VITE_HOSTNAME}:${import.meta.env.VITE_PORT_BACKEND}/api/use/books`, {
                     method: "GET",
                     headers: {
@@ -37,7 +40,6 @@ export default function Books() {
                 });
                 if (res.status === 200) {
                     const response = await res.json();
-                    console.log(response);
                     setDatos(response);
                     return response;
                 }
@@ -46,12 +48,17 @@ export default function Books() {
                 }
             } catch (error) {
                 alert("error");
-            } finally {
-                setLoading(false);
             }
         }
         getAviableBooks();
     }, [])
+
+    useEffect(()=>{
+        if(datos.length > 0){
+            setLoading(false);
+        }
+    },[datos]);
+
     return (
         <>  
             <Box sx={{ padding: { lg: '5% 5%' } }}>
@@ -61,63 +68,69 @@ export default function Books() {
                 </Box>
                 <Box sx={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'center' }}>
                     {
-                        datos.map((info) => (
-                            <>
-                                {info.books ? (
-                                    <>
-                                        {
-                                            info.books.map((book) => (
-                                                <>
-                                                    <Box sx={{ backgroundColor: '#FFEEDD', borderRadius: '20px', width: '330px', height: '554px', margin: '3% 2% 1% 2%' }}>
-                                                        <Box sx={{ margin: '3%', textAlign: 'center', display: 'flex', alignItems: 'center', height: '40px' }}>
-                                                            <p>Editorial: {book.editorial}</p>
+                        loading ? (
+                            <p>cargando...</p>
+                        ) : ( 
+                            
+                            datos.map((info) => (
+                                <>
+                                    {info.books ? (
+                                        <>
+                                            {
+                                                info.books.map((book) => (
+                                                    <>
+                                                        <Box sx={{ backgroundColor: '#FFEEDD', borderRadius: '20px', width: '330px', height: '554px', margin: '3% 2% 1% 2%' }}>
+                                                            <Box sx={{ margin: '3%', textAlign: 'center', display: 'flex', alignItems: 'center', height: '40px' }}>
+                                                                <p>Editorial: {book.editorial}</p>
+                                                            </Box>
+                                                            <Box sx={{ backgroundColor: '#B8B8FF', display: 'flex', justifyContent: 'center', alignItems: 'center',  borderRadius: '20px', height: '355px' }}>
+                                                            {
+                                                                book.imagen == '' ? (<img className='imagenbook' src={defaultImage} alt="" />) : (  <img className='imagenbook' src={book.imagen} alt="" />)
+                                                            }
+                                                            </Box>
+                                                            <Box className="informacion" sx={{ padding: '5% 5% 2% 10% ' }}>{
+                                                                book.disponibilidad ? (<><Box sx={{ backgroundColor: '#02970278', borderRadius: '20px', padding: '2%', textAlign: 'center', width: '24%' }}><p>Disponible</p></Box></>) : (<><Box sx={{ backgroundColor: '#ff5b5b9e', borderRadius: '20px', padding: '2%', textAlign: 'center', width: '24%' }}><p>Ocupado</p></Box></>)
+                                                            }</Box>
+                                                            <Box className="informacion" sx={{ padding: '0% 5% 2% 10% ' }}>
+                                                                <h3>{book.titulo}</h3>
+                                                                <p>Autor: {book.autor}</p>
+                                                                <p>Descripicón: {book.descripcion}</p>
+                                                                <p>Disponibilidad: {book.disponibilidad}</p>
+                                                            </Box>
                                                         </Box>
-                                                        <Box sx={{ backgroundColor: '#B8B8FF', display: 'flex', justifyContent: 'center', alignItems: 'center',  borderRadius: '20px', height: '355px' }}>
-                                                        {
-                                                            book.imagen == '' ? (<img className='imagenbook' src={defaultImage} alt="" />) : (  <img className='imagenbook' src={book.imagen} alt="" />)
-                                                        }
-                                                        </Box>
-                                                        <Box className="informacion" sx={{ padding: '5% 5% 2% 10% ' }}>{
-                                                            book.disponibilidad ? (<><Box sx={{ backgroundColor: '#02970278', borderRadius: '20px', padding: '2%', textAlign: 'center', width: '24%' }}><p>Disponible</p></Box></>) : (<><Box sx={{ backgroundColor: '#ff5b5b9e', borderRadius: '20px', padding: '2%', textAlign: 'center', width: '24%' }}><p>Ocupado</p></Box></>)
-                                                        }</Box>
-                                                        <Box className="informacion" sx={{ padding: '0% 5% 2% 10% ' }}>
-                                                            <h3>{book.titulo}</h3>
-                                                            <p>Autor: {book.autor}</p>
-                                                            <p>Descripicón: {book.descripcion}</p>
-                                                            <p>Disponibilidad: {book.disponibilidad}</p>
-                                                        </Box>
-                                                    </Box>
-                                                </>
-                                            ))
-                                        }
-                                    </>
-                                ) : (
-                                    <>
-                                        <Box sx={{ backgroundColor: '#FFEEDD', borderRadius: '20px', width: '330px', height: '494px', margin: '3% 2% 0% 2%' }}>
-                                            <Box sx={{ backgroundColor: '#B8B8FF', borderRadius: '20px', height: '355px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                                {
-                                                    info.imagen == '' ? (<img className='imagenbook' src={defaultImage} alt="" />) : (  <img className='imagenbook' src={info.imagen} alt="" />)
-                                                }
-                                               
+                                                    </>
+                                                ))
+                                            }
+                                        </>
+                                    ) : (
+                                        <>
+                                            <Box sx={{ backgroundColor: '#FFEEDD', borderRadius: '20px', width: '330px', height: '494px', margin: '3% 2% 0% 2%' }}>
+                                                <Box sx={{ backgroundColor: '#B8B8FF', borderRadius: '20px', height: '355px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                                    {
+                                                        info.imagen == '' ? (<img className='imagenbook' src={defaultImage} alt="" />) : (  <img className='imagenbook' src={info.imagen} alt="" />)
+                                                    }
+                                                   
+                                                </Box>
+                                                <Box className="informacion" sx={{ padding: '5% 5% 2% 10% ' }}>{
+                                                    info.disponibilidad ? (<><Box sx={{ backgroundColor: '#02970278', borderRadius: '20px', padding: '2%', textAlign: 'center', width: '24%' }}><p>Disponible</p></Box></>) : (<><Box sx={{ backgroundColor: '#ff5b5b9e', borderRadius: '20px', padding: '2%', textAlign: 'center', width: '24%' }}><p>Ocupado</p></Box></>)
+                                                }</Box>
+                                                <Box className="informacion" sx={{ padding: '0% 5% 2% 10% ' }}>
+    
+                                                    <h3>{info.titulo}</h3>
+                                                    <p>Autor: {info.author}</p>
+                                                    <p>Descripicón: {info.descripcion}</p>
+    
+    
+                                                </Box>
                                             </Box>
-                                            <Box className="informacion" sx={{ padding: '5% 5% 2% 10% ' }}>{
-                                                info.disponibilidad ? (<><Box sx={{ backgroundColor: '#02970278', borderRadius: '20px', padding: '2%', textAlign: 'center', width: '24%' }}><p>Disponible</p></Box></>) : (<><Box sx={{ backgroundColor: '#ff5b5b9e', borderRadius: '20px', padding: '2%', textAlign: 'center', width: '24%' }}><p>Ocupado</p></Box></>)
-                                            }</Box>
-                                            <Box className="informacion" sx={{ padding: '0% 5% 2% 10% ' }}>
-
-                                                <h3>{info.titulo}</h3>
-                                                <p>Autor: {info.author}</p>
-                                                <p>Descripicón: {info.descripcion}</p>
-
-
-                                            </Box>
-                                        </Box>
-                                    </>
-                                )
-                                }
-                            </>
-                        ))
+                                        </>
+                                    )
+                                    }
+                                </>
+                            ))
+                        )
                     }
+                   
                 </Box>
             </Box>
         </>
